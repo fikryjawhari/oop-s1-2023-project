4e50cbf2ba749ec0366ce77daae6f110049a2b70
ohnihili <bpfsignorelli@gmail.com>
2023-05-04 19:33:50 +0930

    test push

--- Pawn.h
+++ Pawn.h
@@ -9,6 +9,7 @@ class Pawn : public Piece {
 public:
     bool validMove(int x_pos, int y_pos);   // check if move is valid
     string movePiece(int x_pos, int y_pos); // move piece and return new location, should implement as a x,y instead of string
+    int number = 1;
 };
 
 #endif // PAWN_H
