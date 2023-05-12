94a561db6e9fa5059fd78350ffcf207f6f2b250f
ohnihili <bpfsignorelli@gmail.com>
2023-05-12 14:47:36 +0930

    further sfml stuff

---
+++ GameStates/loadGS1.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow Chess(sf::VideoMode(800,800), "Chess Puzzles");
+
+    sf::Texture texMenu;
+    sf::Texture texGS1;
+    sf::Texture texGS2;
+
+    sf::Sprite spMenu;
+    sf::Sprite spGS1;
+    sf::Sprite spGS2;
+
+    if (!texMenu.loadFromFile("Menu.png"))
+        {
+            std::cout << "Could not load image" << std::endl;
+
+            return 1;
+        }
+        else
+        {
+            spMenu.setTexture(texMenu);
+        }
+
+    if (!texGS1.loadFromFile("GS1.PNG"))
+        {
+            std::cout << "Could not load image" << std::endl;
+
+            return 1;
+        }
+        else
+        {
+            spGS1.setTexture(texGS1);
+        }
+
+    if (!texGS2.loadFromFile("GS2.PNG"))
+        {
+            std::cout << "Could not load image" << std::endl;
+
+            return 1;
+        }
+        else
+        {
+            spGS2.setTexture(texGS2);
+        }
+
+    while (Chess.isOpen())
+    {
+        Chess.clear(); 
+        Chess.draw(spMenu);
+        Chess.display();
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (Chess.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+            {
+                Chess.close();
+            } 
+            
+            if (event.type == sf::Event::TextEntered)
+            {
+                if (event.text.unicode < 128)
+                {
+                    Chess.clear(); 
+                    Chess.draw(spGS1);
+                    Chess.display(); 
+                }
+
+            }
+        }
+
+    }
+
+    return 0;
+}   
